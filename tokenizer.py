c5f19b41588352d8a08d0db5ace4210ca04e48bb
KARTIK SHARMA <kartik2003dav@gmail.com>
2025-07-01 19:04:53 +0530

    implementation

--- tokenizer.py
+++
@@ -1,7 +0,0 @@
-# tokenizer.py
-from transformers import GPT2Tokenizer
-
-def get_tokenizer():
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    tokenizer.pad_token = tokenizer.eos_token
-    return tokenizer
